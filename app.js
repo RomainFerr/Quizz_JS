bdae1793839cca762811f6f4acc756d376997de1
FERREIRAR <romain.cdesafer@gmail.com>
2023-04-03 11:45:56 +0200

    03/04 -

--- app.js
+++ app.js
@@ -13,5 +13,30 @@ const getThemes = ()=>{
 // je vais rendre disponible cette fonction dans d'autres modules
 
 getThemes()
-    .then(result=>console.log(result))
+    .then(result=>displayThemes(result))
     .catch(error=> console.log(error))
+
+const sectionChoisirTheme = document.querySelector('#ChoisirLeTheme')
+
+const displayThemes = (themes) =>{
+
+    const div = document.querySelector('#Choisir')
+
+
+    themes.forEach(theme=>{
+
+        const p_theme = document.createElement('p');
+        p_theme.textContent = theme.libelle
+        p_theme.classList.add("shadowfort","btn","btn-primary","m-3","rounded-pill","bg-gradient","border", "border-primary","text-white","opacity-75","col-10",'col-lg-4','col-xl-2')
+        div.appendChild(p_theme)
+    })
+}
+
+
+document.onclick = function(event) {
+    if (event===undefined) event= window.event;
+
+    var target= 'target' in event? event.target : event.srcElement;
+    console.log(target)
+}
+
